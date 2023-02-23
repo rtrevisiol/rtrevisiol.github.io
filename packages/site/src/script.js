c3de77bd64295094b3e946f4b9e20d5e8a8611a0
Ruggero <ruggeroa1000@gmail.com>
2023-02-23 17:01:10 +0100

    predisposizione scrittura file

--- packages/site/src/script.js
+++ packages/site/src/script.js
@@ -35,7 +35,7 @@ const calcolo = (n, k) => {
   loading(false);
 }
 
-const combinazioni = function (k, n) {
+/* const combinazioni = function (k, n) {
 
   // n -> [a] -> [[a]]
   function comb(n, lst) {
@@ -86,13 +86,9 @@ const combinazioni = function (k, n) {
   var container = document.getElementById('container');
   container.appendChild(a);
 
-  /* return fnMemoized(n, lstRange)
-
-  .map(function (x) {
-    return x.join(' ');
-  }).join('\n'); */
   return false;
-}
+} 
+*/
 
 const stampa = () => {
   var printContents = document.getElementById("IDStampa").innerHTML; //ID del div da stampare
@@ -100,4 +96,78 @@ const stampa = () => {
   document.body.innerHTML = printContents; // Setta contenuto della pagina con il div da stampare
   window.print(); //Stampa
   document.body.innerHTML = originalContents; // ripristina contenuto della pagina originale
+}
+
+/* function memoize(func) {
+  var cache = {};
+  return function () {
+    var argsAsString = JSON.stringify(arguments);
+    cache[argsAsString] = cache[argsAsString] || func.apply(func, arguments);
+    return cache[argsAsString];
+  };
+}
+
+
+const trovaProxNum = (arrIni, n, k, num, posizione) => {
+  let arrUpdated = [];
+  for (let i0 = 0; i0 < n; i0++) {
+    arrUpdated[i0] = [];
+    arrUpdated[i0][0] = num;
+    for (let i = 1; i < k; i++) {
+      if (Math.round(arrUpdated[i0][i - 1]) + 1 <= n) {
+        let numProx = Math.round(arrUpdated[i0][i - 1])+1
+        arrUpdated[i0].push(numProx)
+      }
+    }
+  }
+  return arrUpdated;
+}
+
+var memoizedTrovaProxNum = memoize(trovaProxNum);
+
+const combXNum = (n, k, num, posizione) => {
+  let data = memoizedTrovaProxNum([[Math.round(num)]], Math.round(n), Math.round(k), Math.round(num), Math.round(posizione));
+
+  var a = document.createElement('a');
+  //a.href = 'data:' + data;
+  a.href = 'data:' + "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
+  //a.href = 'data:' + "text/csv;charset=utf-8," + encodeURIComponent(data);
+  // a.download = 'data-[' + num + '].csv';
+  a.download = 'data-[' + num + '].json';
+  // a.innerHTML = 'download CSV [' + num + ']<br/>';
+  a.innerHTML = 'download JSON [' + num + ']<br/>';
+  var container = document.getElementById('container');
+  container.appendChild(a);
+}
+
+const combinazioni = function (n, k) {
+  loading(true);
+  let arrayNumeri = Array.from({ length: n }, (_, i) => i + 1);
+  arrayNumeri.map((num) => {
+    combXNum(n, k, num, 0);
+    // console.log(num);
+  });
+  loading(false);
+} */
+
+function bitprint(u) {
+  var s = [];
+  for (var n = 0; u; ++n, u >>= 1)
+    if (u & 1) s.push(n+1);
+  return s;
+}
+function bitcount(u) {
+  for (var n = 0; u; ++n, u = u & (u - 1));
+  return n;
+}
+function combinazioni(n, c) {
+  // var s = [];
+  for (var u = 0; u < 1 << n; u++){
+    if (bitcount(u) == c){
+      console.log(bitprint(u))
+    }
+      // s.push(bitprint(u))
+  }
+  // window.pippo = s.sort();
+  // return window.pippo;
 }
