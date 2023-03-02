dd656de10fd98a70a2337fc08276c09f5ae9cbb1
Ruggero <ruggeroa1000@gmail.com>
2023-03-02 10:10:27 +0100

    codice per combinazione corretta

--- packages/site/src/script.js
+++ packages/site/src/script.js
@@ -6,8 +6,8 @@ let ultimiNumeri = () => {
   //https://www.gntn-pgd.it/gntn-info-ext-web/rest/gioco/statistiche/superenalotto/sestine/daticompleti?idPartner=GIOCHINUMERICI_INFO
   //per recuperare i dati : temp1.dati.gruppi[0].elementi.filter(numero => numero.valori[1].valore == "0").map((el)=>  el.simbolo)
   function returnVal(combo) {
-    ultimaCombo = JSON.parse(combo).dati.gruppi[0].elementi.filter(numero => numero.valori[1].valore == "0").map((el)=>  el.simbolo);
-    penultimaCombo = JSON.parse(combo).dati.gruppi[0].elementi.filter(numero => numero.valori[1].valore == "1").map((el)=>  el.simbolo);
+    ultimaCombo = JSON.parse(combo).dati.gruppi[0].elementi.filter(numero => numero.valori[1].valore == "0").map((el) => el.simbolo);
+    penultimaCombo = JSON.parse(combo).dati.gruppi[0].elementi.filter(numero => numero.valori[1].valore == "1").map((el) => el.simbolo);
   }
   let file = "https://www.gntn-pgd.it/gntn-info-ext-web/rest/gioco/statistiche/superenalotto/sestine/daticompleti?idPartner=GIOCHINUMERICI_INFO"
   fetch(file)
@@ -38,22 +38,19 @@ const factorial = (x) => {
 }
 
 const calcolo = (n, k) => {
-  loading(true);
-  //console.log(n,k)
+  let res;
   try {
-    window.pippo = "";
-    let res;
     res = factorial(n) / (factorial(k) * factorial(n - k));
-    console.log(res);
   } catch (err) {
     console.error(err);
   }
-  /*     try {
-        window.pippo = combinazioni(n,k);
-      } catch(err){
-        console.log(err);
-      } */
-  loading(false);
+  document.getElementById("result").innerHTML = "ci sono " + res + " combinazioni senza ripetizioni";
+  confirm("Ci sono "+res+" combinazioni senza ripetizione.");
+  return res;
+}
+
+const percentualeAvanzamento = (n) => {
+  document.getElementById("percentuale").innerHTML = "calcolo in corso ==> " + Math.round(n) + "%";
 }
 
 /* const combinazioni = function (k, n) {
@@ -180,7 +177,7 @@ const controllo = (arrayCombo) => {
       arr[i - 1] ? diff.push((element - arr[i - 1])) : void (0);
     });
     let uniqueItems = [...new Set(diff)]
-    let result = uniqueItems.length > 3;//accetto solo 3 numeri che si distanziano tra loro per lo stesso valore
+    let result = uniqueItems.length >= (combo.length / 2);//accetto solo 3 numeri che si distanziano tra loro per lo stesso valore
     return result;
   }
   let decine = (combo) => {
@@ -190,11 +187,19 @@ const controllo = (arrayCombo) => {
       dec.push(decina);
     });
     let uniqueItems = [...new Set(dec)]
-    let result = uniqueItems.length > 3;//accetto solo 3 numeri con la stessa decina
+    let result = uniqueItems.length >= (combo.length / 2);//accetto solo 3 numeri con la stessa decina
     return result;
   }
-
-  return (differenze(arrayCombo) && decine(arrayCombo));
+  let estrazioni = (combo) => {
+    let differenzeUltima = combo.filter(x => ultimaCombo.indexOf(x) === -1);
+    let differenzePenultima = combo.filter(x => penultimaCombo.indexOf(x) === -1);
+    return (differenzeUltima < (combo.length / 2) && differenzePenultima < (combo.length / 2));
+  }
+  // let result = (differenze(arrayCombo) && decine(arrayCombo));
+  // let result = differenze(arrayCombo);
+  // let result = decine(arrayCombo);
+  let result = true;
+  return result;
 }
 
 function bitprint(u) {
@@ -208,6 +213,8 @@ function bitcount(u) {
   return n;
 }
 function combinazioni(n, c) {
+  let combTot = calcolo(n, c);
+  let contaCicli = 0;
   let queryPulizia = "DROP TABLE combinazioni;";
   db.transaction(function (tx) {
     tx.executeSql(queryPulizia);
@@ -228,10 +235,13 @@ function combinazioni(n, c) {
   for (let i = 1; i < (Math.round(c) + 1); i++) {
     i == c ? query_scrittura += "numero" + i : query_scrittura += "numero" + i + ",";
   }
-  query_scrittura += ") VALUES("
+  query_scrittura += ") VALUES(";
 
-  for (var u = 0; u < 1 << n; u++) {
+/*   for (var u = 0; u < 1 << n; u++) {
     if (bitcount(u) == c) {
+      contaCicli++;
+      let perc = (contaCicli / combTot) * 100;
+      percentualeAvanzamento(perc);
       if (controllo(bitprint(u))) {
         let queryEseguibile = query_scrittura;
         queryEseguibile += bitprint(u).toString() + ");";
@@ -239,11 +249,72 @@ function combinazioni(n, c) {
         db.transaction(function (tx) {
           tx.executeSql(queryEseguibile);
         });
-        // console.log(bitprint(u))
       }
     }
-    // s.push(bitprint(u))
-  }
+  } */
+  let arrNumb = Array.from({ length: n }, (value, index) => index+1);
+  console.log(k_combinations(arrNumb,c));
+
+  console.log("cicli totali : " + contaCicli)
   // window.pippo = s.sort();
   // return window.pippo;
+}
+
+
+//FONTE ORIGINALE: https://gist.github.com/axelpale/3118596
+function k_combinations(set, k) {
+	var i, j, combs, head, tailcombs;
+	
+	// There is no way to take e.g. sets of 5 elements from
+	// a set of 4.
+	if (k > set.length || k <= 0) {
+		return [];
+	}
+	
+	// K-sized set has only one K-sized subset.
+	if (k == set.length) {
+		return [set];
+	}
+	
+	// There is N 1-sized subsets in a N-sized set.
+	if (k == 1) {
+		combs = [];
+		for (i = 0; i < set.length; i++) {
+			combs.push([set[i]]);
+		}
+		return combs;
+	}
+	
+	// Assert {1 < k < set.length}
+	
+	// Algorithm description:
+	// To get k-combinations of a set, we want to join each element
+	// with all (k-1)-combinations of the other elements. The set of
+	// these k-sized sets would be the desired result. However, as we
+	// represent sets with lists, we need to take duplicates into
+	// account. To avoid producing duplicates and also unnecessary
+	// computing, we use the following approach: each element i
+	// divides the list into three: the preceding elements, the
+	// current element i, and the subsequent elements. For the first
+	// element, the list of preceding elements is empty. For element i,
+	// we compute the (k-1)-computations of the subsequent elements,
+	// join each with the element i, and store the joined to the set of
+	// computed k-combinations. We do not need to take the preceding
+	// elements into account, because they have already been the i:th
+	// element so they are already computed and stored. When the length
+	// of the subsequent list drops below (k-1), we cannot find any
+	// (k-1)-combs, hence the upper limit for the iteration:
+	combs = [];
+	for (i = 0; i < set.length - k + 1; i++) {
+		// head is a list that includes only our current element.
+		head = set.slice(i, i + 1);
+		// We take smaller combinations from the subsequent elements
+		tailcombs = k_combinations(set.slice(i + 1), k - 1);
+		// For each (k-1)-combination we join it with the current
+		// and store it to the set of k-combinations.
+		for (j = 0; j < tailcombs.length; j++) {
+			combs.push(head.concat(tailcombs[j]));
+		}
+	}
+	return combs;
 }
